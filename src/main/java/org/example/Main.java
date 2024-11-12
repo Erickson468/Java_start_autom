9495a9fa0458cf52c0bf594a41629737c504dbc2
Erickson <117378409+Erickson468@users.noreply.github.com>
2024-11-12 22:38:58 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("შეიყვანე სასურველი რიცხვი, თუ შეიყვან 0 პროცესი დასრულდება");
+        int number = scanner.nextInt();
+    }
+}
