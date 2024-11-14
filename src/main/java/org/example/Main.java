b05a2aa5db873529ef67a8e0e8df42d2b9664865
Erickson <117378409+Erickson468@users.noreply.github.com>
2024-11-14 22:53:32 +0400

    Task1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,41 @@
+
 package org.example;
+
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("შეიყვანე სასურველი რიცხვი, თუ შეიყვან 0 პროცესი დასრულდება");
-        int number = scanner.nextInt();
+        Scanner Answer = new Scanner(System.in);
+        int i = 2;
+        System.out.println("უპასუხეთ კითხვას: რომელქუდა შეჭამა მგელმა?");
+        String userAnswer = Answer.nextLine();
+        while (i>0){
+            if (userAnswer.equals("წითელქუდა")){
+                System.out.println("თქვენ შეიყვანეთ სწორი პასუხი, გილოცავთ " + userAnswer);
+                break;
+            }
+            else {
+                System.out.println("თქვენ შეიყვანეთ \"" + userAnswer+ "\" ცადეთ თავიდან, დაგრჩათ "+i+" ცდა: ");
+                userAnswer = Answer.nextLine();
+                i--;
+                if (i ==0){
+                    System.out.println("თქვენ გამოგელიათ ცდების ოდენობა");
+                }
+            }
+        }
+
+        //პირველი ვარიანტი რაც დავწერე
+//        while (!userAnswer.equals("წითელქუდა") && i>0){
+//            i--;
+//            System.out.println("თქვენ შეიყვანეთ \"" + userAnswer+ "\" ცადეთ თავიდან, დაგრჩათ "+i+" ცდა: ");
+//            userAnswer = Answer.nextLine();
+//        }
+//        if(i==0) {
+//            System.out.println("თქვენ გამოგელიათ ცდების ოდენობა");
+//        }
+//        else {
+//            System.out.println("თქვენ შეიყვანეთ სწორი პასუხი, გილოცავთ " + userAnswer);
+//        }
     }
 }
