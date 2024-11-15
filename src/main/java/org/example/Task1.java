0b8402cf436b19638ec255e9bf7bddfab241b8a9
Erickson <117378409+Erickson468@users.noreply.github.com>
2024-11-15 18:39:41 +0400

    Homework1

---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Task1 {
+    public static void main(String[] args) {
+    int j = 0;
+    int [] array = {1, 2, 3, 4, 5};
+    for (int i =0;i <array.length;i++){
+        j+=array[i];
+    }
+    System.out.println(j);
+}
+}
---
+++ src/main/java/org/example/Task3.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Task3 {
+    public static void main(String[] args) {
+    String [] array = {"Erti","Ori","Sami","Otxi"};
+    for (int i= (array.length-1); i>=0; i--){
+        System.out.println(array[i]);
+    }
+    }
+}
